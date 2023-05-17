a3de35214f61a8c900f9eacdd7e25ab5c5f8884a
Devansh <devanshkhullar11@gmail.com>
2023-05-17 19:42:11 +0530

    added functionality to fetch data every 2 seconds

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,110 +3,129 @@ import ReactApexChart from "react-apexcharts";
 import axios from "axios";
 
 export default function Body() {
+  const [data, setData] = useState([]);
 
-const [data,setData]=useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        await axios
+        .get("https://iot-dashboard.onrender.com/getdata")
+        .then((res) => {
+          console.log("api call");
+          setData(res?.data);
+        })
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    const interval = setInterval(() => {
+      getData();
+    }, 2000);
 
-const getData= async ()=>{
-    await axios.get('https://iot-dashboard.onrender.com/getdata').then((res)=>{setData(res.data)}).catch((err)=>{console.log(err)});
-}
+    return () => {
+      clearInterval(interval);
+    };
 
-useEffect(() => {
-    getData();
-}, [])
+  }, []);
 
-console.log(data);
-const newone =[];
-var serie = data[data.length -1];
-const final = serie?.value;
-console.log(final);
-const ffinal = Math.abs(((final*100 + 85400000) / 68400000) * 100);
-console.log(ffinal)
-newone.push(Math.round(ffinal))
+  console.log(data);
+  const newone = [];
+  var serie = data[data?.length - 1];
+  const final = serie?.value;
+  console.log(final);
+  const ffinal = Math.abs(((final * 100 + 85400000) / 68400000) * 100);
+  console.log(ffinal);
+  newone.push(Math.round(ffinal));
 
-  const [options,setOptions]=useState({
+  const [options, setOptions] = useState({
     chart: {
       height: 350,
-      type: 'radialBar',
+      type: "radialBar",
       toolbar: {
-        show: true
-      }
+        show: true,
+      },
     },
     plotOptions: {
       radialBar: {
         startAngle: -135,
         endAngle: 225,
-         hollow: {
+        hollow: {
           margin: 0,
-          size: '70%',
-          background: '#fff',
+          size: "70%",
+          background: "#fff",
           image: undefined,
           imageOffsetX: 0,
           imageOffsetY: 0,
-          position: 'front',
+          position: "front",
           dropShadow: {
             enabled: true,
             top: 3,
             left: 0,
             blur: 4,
-            opacity: 0.24
-          }
+            opacity: 0.24,
+          },
         },
         track: {
-          background: '#fff',
-          strokeWidth: '67%',
+          background: "#fff",
+          strokeWidth: "67%",
           margin: 0, // margin is in pixels
           dropShadow: {
             enabled: true,
             top: -3,
             left: 0,
             blur: 4,
-            opacity: 0.35
-          }
+            opacity: 0.35,
+          },
         },
-    
+
         dataLabels: {
           show: true,
           name: {
             offsetY: -10,
             show: true,
-            color: '#888',
-            fontSize: '17px'
+            color: "#888",
+            fontSize: "17px",
           },
           value: {
-            formatter: function(val) {
+            formatter: function (val) {
               return parseInt(val);
             },
-            color: '#111',
-            fontSize: '36px',
+            color: "#111",
+            fontSize: "36px",
             show: true,
-          }
-        }
-      }
+          },
+        },
+      },
     },
     fill: {
-      type: 'gradient',
+      type: "gradient",
       gradient: {
-        shade: 'dark',
-        type: 'horizontal',
+        shade: "dark",
+        type: "horizontal",
         shadeIntensity: 0.5,
-        gradientToColors: ['#FF0000'],
+        gradientToColors: ["#FF0000"],
         inverseColors: true,
         opacityFrom: 1,
         opacityTo: 1,
-        stops: [0, 100]
-      }
+        stops: [0, 100],
+      },
     },
     stroke: {
-      lineCap: 'round'
+      lineCap: "round",
     },
-    labels: ['Tank Level (in %)'],
-  })
+    labels: ["Tank Level (in %)"],
+  });
 
   return (
     <>
       <div className="bg-gray-100 flex justify-center h-screen ">
         <div className="bg-white w-1/3 h-1/2 mt-20 rounded-2xl shadow-lg flex justify-center items-center ">
-        <ReactApexChart options={options} series={newone} type="radialBar" height={350} />
+          <ReactApexChart
+            options={options}
+            series={newone}
+            type="radialBar"
+            height={350}
+          />
         </div>
       </div>
     </>
